7a1843cc20ba5ed25b6f9243306032525af45538
Amund Halgunset <amundhalgunset@gmail.com>
2024-09-17 23:12:36 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ import "./css/style.css";
 
 import router from "./js/router";
 
-await router(window.location.pathname);
+async function init() {
+    await router(window.location.pathname);
+  }
+
+init();
