01c3bd54cd6f60ddee97e0aab63ccb329c586c20
Amund Halgunset <amundhalgunset@gmail.com>
2024-09-24 23:38:39 +0200

    login startup

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,5 +1,6 @@
 import { onLogin } from "../../ui/auth/login";
+import { login } from "../../api/auth/login";
 
 const form = document.forms.login;
 
-form.addEventListener("submit", onLogin);
+form.addEventListener("submit", onLogin, login);
